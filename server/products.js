628070c06ddcfe3740775929eb4db31d5a6eb740
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-09-17 04:33:23 -0400

    Renames the 'erd.png' file and adds in a new 'products.js' file for seed data

---
+++ server/products.js
@@ -0,0 +1,46 @@
+module.exports = [
+  {
+    title: 'Jogger Stroller - Blue',
+    description: 'A gently-used jogger stroller',
+    price: 40.0,
+    quantity: 1,
+    image:
+      'https://res.cloudinary.com/tico-cloudinary/image/upload/v1568708733/lucys_store/jogger_stroller_blue_yn04au.jpg',
+  },
+  {
+    title: 'Jogger Stroller - Red',
+    description:
+      'A gently-used jogger stroller that comes with a matching baby carrier',
+    price: 60.0,
+    quantity: 1,
+    image:
+      'https://res.cloudinary.com/tico-cloudinary/image/upload/v1568708740/lucys_store/jogger_stroller_red_bn8kob.jpg',
+  },
+  {
+    title: 'Foot Massager',
+    description:
+      'A relaxation-inducing foot massager with a built-in foot warming feature',
+    price: 70.0,
+    quantity: 1,
+    image:
+      'https://res.cloudinary.com/tico-cloudinary/image/upload/v1568708731/lucys_store/foot_massager_mhaffn.jpg',
+  },
+  {
+    title: 'Back Massager',
+    description:
+      'A well-taken care of back massager that will take away those knots',
+    price: 90.0,
+    quantity: 1,
+    image:
+      'https://res.cloudinary.com/tico-cloudinary/image/upload/v1568708726/lucys_store/back_massager_wquvzq.jpg',
+  },
+  {
+    title: 'Schwinn Airdyne Exercise Bike',
+    description:
+      "A used, fully-functioning exercise bike that will help you lose that extra 10 lbs you've been targeting",
+    price: 75.0,
+    quantity: 1,
+    image:
+      'https://res.cloudinary.com/tico-cloudinary/image/upload/v1568708723/lucys_store/airdyne_seuv5z.jpg',
+  },
+];
