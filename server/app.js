f3cd070cfa4bd5f73b84647dd9c753fb199b4909
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-09-17 00:27:13 -0400

    Adds 'app.use(cors());' above GET '/' endpoint to add 'Access-Control-Allow-Origin' header to the responses that are going out from the server so that other origins can make requests to this server

--- server/app.js
+++ server/app.js
@@ -10,6 +10,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.json({
