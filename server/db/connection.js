947352db3163b45ddba574836dfd823f7e009bf5
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-09-17 04:56:36 -0400

    Creates a 'connection.js' file to house configurations for how to connect to local DB

---
+++ server/db/connection.js
@@ -0,0 +1,6 @@
+// runs the local database
+const environment = process.env.NODE_ENV || 'development';
+const config = require('../knexfile');
+const environmentConfig = config[environment];
+const knex = require('knex');
+const connection = knex(environmentConfig);
